83a3adfc44e4995e5fcb15ca1ecae8b7aebdced2
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-04-09 21:31:51 +0300

    upload

--- api/main.py
+++ api/main.py
@@ -1,26 +1,9 @@
-from telegram.ext import Updater, CommandHandler
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-def start(update, context):
-    update.message.reply_text('Hello! I am your Telegram bot.')
-
-def help(update, context):
-    update.message.reply_text('You can use /start to begin and /help to get assistance.')
-
-def handler(event, context):
-    # Initialize bot
-    updater = Updater(getenv('BOT_TOKEN'), use_context=True)
-    dp = updater.dispatcher
-
-    # Register handlers
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C
-    updater.idle()
+from sanic import Sanic
+from sanic.response import json
+app = Sanic()
+ 
+ 
+@app.route('/')
+@app.route('/<path:path>')
+async def index(request, path=""):
+    return json({'hello': path})
